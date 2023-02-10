5f7df695a85c799b01ae4d78b009dbf627cd609f
adityaineuron <adityashinde@ineuron.ai>
2023-02-10 17:21:12 +0530

    updates

--- resume_keyword/components/data_transformation.py
+++ resume_keyword/components/data_transformation.py
@@ -111,7 +111,7 @@ class DataTransformation:
                     TEST_RESUME_TXT_FILE_FOLDER_NAME,
                     f"image_txt_{str(i)}.txt",
                 )
-                with open(fname, "w") as file:
+                with open(fname, "w", encoding='utf-8') as file:
                     file.write(text)
 
         except Exception as e:
